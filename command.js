0e5c56ffb2897a802fc372ac60eee6f6c41cde33
thomas <thom.schell@gmail.com>
2015-09-22 14:01:06 +0200

    improved script

--- command.js
+++ command.js
@@ -34,15 +34,21 @@ function _playMovie(args) {
 
     // Change args to make them better
     var args = _formatArgs(args);
+    var pattern = args.join('*') + '*';
+    console.log("pattern", pattern);
+
     var files = glob.sync([
-        'files/**',      //include all     files/
-        '!files/x/**',   //then, exclude   files/x/
-        'files/x/z.txt'  //then, reinclude files/x/z.txt
+        'files/**/' + pattern      //include all     files/
     ]);
+
+    console.log("files", files);
 }
 
 function _formatArgs(args) {
-    var formatedArgs = _.map(args, function(arg) {
+    var seasonIndex;
+    var episodIndex;
+
+    var formatedArgs = _.map(args, function(arg, index) {
         // Convert text to numbers
         try {
             arg = text2num(arg);
@@ -51,9 +57,44 @@ function _formatArgs(args) {
         } catch(err) {
             // don't care
         }
+
+        // Replace "season" and "episod"
+        if(arg.toLowerCase() === "season") {
+            seasonIndex = index;
+        }
+        if(arg.toLowerCase() === "episod") {
+            episodIndex = index;
+        }
         return arg;
 
     });
+
+    // If both season and episod are provided (serie), make it standard format
+    if(seasonIndex && episodIndex) {
+        formatedArgs[seasonIndex] = 's';
+        formatedArgs[episodIndex] = 'e';
+
+        /*
+        // if two numbers in a row, it's almost certain to be an episod number with a missing dash
+        var previousArgIsInt = false;
+        var indexesToBeDeleted = [];
+        formatedArgs = _.compact(_.map(formatedArgs, function(arg, index) {
+            var intPattern =  /^([0-9]+)$/;
+            var argIsInt = intPattern.test(arg);
+            if(previousArgIsInt && argIsInt) {
+                arg = formatedArgs[index - 1] = formatedArgs[index - 1] + '-' + arg; // update former argument
+                indexesToBeDeleted = index;
+            }
+            previousArgIsInt = argIsInt;
+            return arg;
+        }));
+
+
+    }
+
+
+
+
     console.log("formated args are", formatedArgs);
     return formatedArgs;
 }
