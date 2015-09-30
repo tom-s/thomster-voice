358a2c2de5fc82c732a12e34444004fcd4a46891
Thomas Schell <thom.schell@gmail.com>
2015-09-30 15:27:40 +0200

    added activation

--- index.js
+++ index.js
@@ -1,26 +1,82 @@
 var _ = require('lodash');
+var exec = require('child_process').exec;
 var utils = require('./utils.js');
 var cmdProcessor = require('./cmdProcessor.js');
 var intentFinder = require('./intentFinder.js');
+var Q = require('q');
 
 var SOUND_FILE = "input.wav";
-var COMPARE_WITH = "wendy.wav";
 
-/*
-intentFinder.get(SOUND_FILE).then(
-    function success(res) {
-        var intentParams = _.map(res.params, function(param, key) {
-            return {
-                key: key,
-                values: _.map(param, function(details) {
-                  return details.value
+var waitingForCommand = false;
+
+function _analyze() {
+    intentFinder.get(SOUND_FILE).then(
+        function success(res) {
+            var intentParams = _.map(res.params, function(param, key) {
+                return {
+                    key: key,
+                    values: _.map(param, function(details) {
+                        return details.value
+                    })
+                };
+            });
+            cmdProcessor.execute(res.intent, res.confidence, intentParams);
+            _sleep();
+        },
+        function error() {
+            utils.speak("Sorry, can you repeat ?");
+            _listen();
+        }
+    );
+}
+
+function _wakeUp() {
+    var deferred = Q.defer();
+    // Check that file is the right duration (to filter out noises)
+    var cmd = "sox " + SOUND_FILE + " -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\\([0-9.]*\\)$#\\1#p'";
+    exec(cmd, function(error, duration, stderr) {
+        duration = parseFloat(duration);
+        console.log("duration", duration);
+        if(duration > 0.5 && duration < 1.5) {
+            deferred.resolve(true);
+        } else {
+            deferred.resolve(false);
+        }
+
+    });
+    return deferred.promise;
+}
+
+/* Wait for a wakeup command */
+function _sleep() {
+    var cmd = 'sox -t alsa default ' + SOUND_FILE + ' silence 1 0.1 5% 1 1 1%';
+    var child = exec(cmd);
+    child.on('close', function(code) {
+        _wakeUp().then(function(wakeUp) {
+            console.log("wake up ? ", wakeUp);
+            if(wakeUp) {
+                utils.speak("Yes ?").then(function success() {
+                    _listen();
                 })
-            };
+            } else {
+                _sleep(); // carry on sleeping
+            }
         });
-        cmdProcessor.execute(res.intent, res.confidence, intentParams);
-    },
-    function error() {
-        utils.speak("Sorry, I can't do that !");
-    }
-);*/
+    });
+}
+
+/* Listen for a command */
+function _listen() {
+    console.log("listen for command");
+    var cmd = 'sox -t alsa default ' + SOUND_FILE + ' silence 1 0.1 5% 1 1.0 5%';
+    var child = exec(cmd);
+    child.on('close', function(code) {
+        _analyze();
+    });
+}
+
+/* Init */
+_sleep();
+
+
 
--- utils.js
+++ utils.js
@@ -1,9 +1,11 @@
 var exec = require('child_process').exec;
+var Q = require('q');
 
 var utils = (function() {
     var lastText = '';
     return {
-        speak: function(text) {
+        speak: function(text, cb) {
+            var deferred = Q.defer();
             var cmd = 'pico2wave -w output.wav " ' + text + '" && aplay output.wav';
             exec(cmd, function(error, stdout, stderr) {
                 // command output is in stdout
@@ -11,7 +13,9 @@ var utils = (function() {
                     console.log('error while executing command ', cmd);
                 }
                 lastText = text;
+                deferred.resolve();
             });
+            return deferred.promise;
         },
         repeat: function() {
             this.speak(lastText);
