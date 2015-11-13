bb015e2a5ae5b7c4bd75dad144bcb58e34ccf546
Thomas Schell <thom.schell@gmail.com>
2015-11-13 21:31:16 +0100

    Added websockets

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var ip = require('ip');
 var clapDetector = require('clap-detector');
 var speaker = require('pico-speaker');
 var orderListener = require('./utils/orderListener.js');
-
+var io = require('socket.io')(8000);
+var ioSocket = null;
 
 /* Config */
 var DEV_IP = '192.168.0.10';
@@ -33,6 +34,15 @@ if(ipAddress !== DEV_IP) {
 
 /* Initalization */
 
+/* Initialize sockets */
+io.on('connection', function(socket){
+    console.log('a user connected');
+    ioSocket = socket;
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
 /* Initialize speaker */
 speaker.init(speakerConfig);
 
@@ -45,7 +55,9 @@ clapDetector.start(clapConfig);
 // Register to multiple claps
 clapDetector.onClaps(3, 2000, function(delay) {
     console.log("3 claps in ", delay, "ms");
+    ioSocket.emit('wakeUp');
     speaker.speak('Yes ?').then(function() {
+        ioSocket.emit('listenForOrder');
         clapDetector.pause();
         console.log("listen for order");
         orderListener.listen(function() {
